eb2bbae9c780f5e521601b1d9d96e5d4d7274d45
Esteban478 <157967976+Esteban478@users.noreply.github.com>
2024-09-13 15:40:55 +0200

    modify font path again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
-
 const manifestForPlugIn: Partial<VitePWAOptions> = {
   registerType: 'autoUpdate',
   manifest:{
